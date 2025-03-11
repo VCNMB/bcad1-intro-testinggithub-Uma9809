b534ecf304c0a2174f63aa4104343eb249840690
Uma9809 <ST10474298@vcconnect.edu.za>
2025-03-11 10:39:16 +0200

    Added new class
    added a new class to the main class

--- PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,11 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("this is my firt GitHub exprience!");
+        
+        newClass nc =  new newClass(); // craeting an object of newClass
+        nc.helloClass(); // have to use variable name nc to commuivate with the class
+        
+        
+        System.out.println(nc.strSecondClassVar); //output variable from 2nd class
     }
 }
